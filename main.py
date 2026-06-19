0f73eea8d2236956fe06198f626d0ef6a678fd22
Offbeatsofa <tuckerdrown77@gmail.com>
2026-06-18 23:50:01 -0600

    rearranged gcode files; refactored code

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from pygrbl_streamer import GrblStreamer
 import keyboard
 import time
 
-g = GrblStreamer(port='COM4', baudrate=115200)
+g = GrblStreamer(port='COM3', baudrate=115200)
 g.progress_callback = lambda pct, cmd: print(f'{pct}%')
 
 def handleInput(keyboardEvent):
+    g.connect
     match keyboardEvent.name:
         case '3':
             print('sending ufo')
@@ -16,11 +17,41 @@ def handleInput(keyboardEvent):
         case '`':
             print('sending tiki')
             g.send_file('tiki_0001.gcode')
+        case '0':
+            print('sending iron man')
+            g.send_file('ironman_0001.gcode')
+        case '1':
+            print('sending spiderman')
+            g.send_file('spiderman_0001.gcode')
+        case '9':
+            print('sending saturn')
+            g.send_file('saturn_0001.gcode')
+        case 'a':
+            print('sending turtle')
+            g.send_file('turtle_0001.gcode')
+        case 'l':
+            print('sending mandalorian')
+            g.send_file('mandalorian_0001.gcode')
+        case 'enter':
+            print('sending stormtrooper')
+            g.send_file('stormtrooper_0001.gcode')
+        case '`':
+            print('sending tiki')
+            g.send_file('tiki_0001.gcode')
+        case 'f2':
+            print('sending bee and flower')
+            print('error: file not added')
+        case 'f8':
+            print('sending flowers')
+            g.send_file('flowers_0001.gcode')
+        case 'home':
+            print('sending bubbles')
+            g.send_file('bubbles_0001.gcode')
+        case _:
+            print(f'key {keyboardEvent.name} not mapped to a file')
+    g.disconnect()
 
-            
+
 keyboard.on_press(handleInput)
-
-g.connect()
 keyboard.wait('esc')
 print('Exiting program')
-g.disconnect()
